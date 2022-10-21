e6f6d8a6205dd0cfc2c46fd9014f53104f32455f
szhan89 <zhangsiweiv@gmail.com>
2022-10-21 10:07:05 -0500

    add function findMidptCourse

--- Karat/Series-CourseSelect.py
+++ Karat/Series-CourseSelect.py
@@ -13,12 +13,15 @@ student_course_pairs_1 = [
   ["94", "Economics"],
   ["25", "Economics"],
 ]
+courses =  [
+    [ 'DataStructures', 'Algorithms' ], [ 'FoundationsOfCS', 'OperatingSystems' ], [ 'ComputerNetworks', 'ComputerArchitecture' ], [ 'Algorithms', 'FoundationsOfCS' ], [ 'ComputerArchitecture', 'DataStructures' ], [ 'SoftwareDesign', 'ComputerNetworks' ]\
+    ]
+
 #####
 
-'''
+'''Question1
 Write a function that takes in a list of (student ID number, course name) pairs and returns, for every pair of students, a list of all courses they share.
 '''
-
 def findPairs(pairs):
     mapVar = collections.defaultdict(list)
     for student, course in pairs:
@@ -35,66 +38,35 @@ def findPairs(pairs):
                     result[keys[i],keys[j]].append(course)
                 
     return result 
-print(findPairs(student_course_pairs_1))
+#print(findPairs(student_course_pairs_1))
 
+'''Question2
+Find the mid point of a course
 '''
-Students may decide to take different "tracks" or sequences of courses in the Computer Science curriculum. There may be more than one track that includes the same course, but each student follows a single linear track from a "root" node to a "leaf" node. In the graph below, their path always moves left to right.
-
-Write a function that takes a list of (source, destination) pairs, and returns the name of all of the courses that the students could be taking when they are halfway through their track of courses.
-
-Sample input:
-all_courses = [
-    ["Logic", "COBOL"],
-    ["Data Structures", "Algorithms"],
-    ["Creative Writing", "Data Structures"],
-    ["Algorithms", "COBOL"],
-    ["Intro to Computer Science", "Data Structures"],
-    ["Logic", "Compilers"],
-    ["Data Structures", "Logic"],
-    ["Creative Writing", "System Administration"],
-    ["Databases", "System Administration"],
-    ["Creative Writing", "Databases"],
-    ["Intro to Computer Science", "Graphics"],
-]
-
-Sample output (in any order):
-          ["Data Structures", "Creative Writing", "Databases", "Intro to Computer Science"]
-
-All paths through the curriculum (midpoint *highlighted*):
-
-*Intro to C.S.* -> Graphics
-Intro to C.S. -> *Data Structures* -> Algorithms -> COBOL
-Intro to C.S. -> *Data Structures* -> Logic -> COBOL
-Intro to C.S. -> *Data Structures* -> Logic -> Compiler
-Creative Writing -> *Databases* -> System Administration
-*Creative Writing* -> System Administration
-Creative Writing -> *Data Structures* -> Algorithms -> COBOL
-Creative Writing -> *Data Structures* -> Logic -> COBOL
-Creative Writing -> *Data Structures* -> Logic -> Compilers
-
-Visual representation:
-
-                    ____________
-                    |          |
-                    | Graphics |
-               ---->|__________|
-               |                          ______________
-____________   |                          |            |
-|          |   |    ______________     -->| Algorithms |--\     _____________
-| Intro to |   |    |            |    /   |____________|   \    |           |
-| C.S.     |---+    | Data       |   /                      >-->| COBOL     |
-|__________|    \   | Structures |--+     ______________   /    |___________|
-                 >->|____________|   \    |            |  /
-____________    /                     \-->| Logic      |-+      _____________
-|          |   /    ______________        |____________|  \     |           |
-| Creative |  /     |            |                         \--->| Compilers |
-| Writing  |-+----->| Databases  |                              |___________|
-|__________|  \     |____________|-\     _________________________
-               \                    \    |                       |
-                \--------------------+-->| System Administration |
-                                         |_______________________|
-
-Complexity analysis variables:
-
-n: number of pairs in the input
-'''
+def findMidptCourse(coures):
+    map_ = collections.defaultdict(str)
+    count = collections.defaultdict(int)
+    startKey = str()
+    #build map
+    for cur, next in courses:
+        map_[cur]=next
+        count[cur]+=1
+        count[next]+=1
+        
+    #find start course
+    for key in count:
+        if count[key]==1 and key in map_:
+            startKey=key
+            break
+    
+    midpt = len(count)//2
+    
+    pt = startKey
+    result = str()
+    while midpt>0:
+        result = map_[pt]
+        pt = map_[pt]
+        midpt-=1
+    
+    return result
+print(findMidptCourse(courses))
