7e4256817a62384861c3d341e37fc8b47844f87d
szhan89 <zhangsiweiv@gmail.com>
2022-10-21 09:45:49 -0500

    Modified the Series-Calculator.py

--- Karat/Series-Calculator.py
+++ Karat/Series-Calculator.py
@@ -6,71 +6,60 @@ expression_2 = "2-(3-999)"
 
 '''
 Given a string that represent an arithmetic operator with numbers and operators i.e "2+3-999". DO NOT INCLUDE parenthesis. Return the calculation result.
+1.traverse expression
+2.if minus sign then set the next number to negative
+3.add all the number while travese
 '''
-def calculator(expression) -> int:
-    if(len(expression) == 0):
-        return 0
-    signed = 1
-    ans = 0
-    i = 0
-    
-    ##iterate through each character of the string
-    while i < len(expression):
-        ##if it is a digit, find the entire number with while loop
+def calcNoParen(expression):
+    i,ans,sign=0,0,1
+    len_=len(expression)
+    while i < len_:
         if(expression[i].isdigit()):
-            num=expression[i]
-            while i + 1 < len(expression) and expression[i + 1].isdigit():
-                i += 1
-                num += expression[i]
-            ans += signed * int(num)#store the number to our answer and do the plus operation with whatever sign in front of a number
-
-        ##determine the sign for the following number
+            num = expression[i]
+            while(i+1 < len_ and expression[i+1].isdigit() ):
+                num+=expression[i+1]
+                i+=1
+            ans+=int(num)*sign
+        
         elif(expression[i]=="+"):
-            signed = 1
-        ##determine the sign for the following number
+            sign=1
+            
         elif(expression[i]=="-"):
-            signed = -1
+            sign=-1
         i+=1
     return ans
-
-print(calculator(expression))
+#print(calcNoParen(expression))
 
 '''
 Given a string that represent an arithmetic operator with numbers and operators i.e "2+3-999". INCLUDE parenthesis. Return the calculation result.
+1.traverse expression
+2.if minus sign then set the next number to negative
+3.if ( then push the current calculated value to stack, then push the sign, start a new calculation and reset sign to 1
+4.if ) then pop the sign to time with the current calculated value, then add with calculated value out side of the parenthesis
+3.add operation for all number while travese
 '''
-def calculator2(expression) -> int:
-    if(len(expression) == 0):
-        return 0
-    signed = 1
-    ans = 0
-    i = 0
-    stack=[]
-    ##iterate through each character of the string
-    while i < len(expression):
-        ##if it is a digit, find the entire number with while loop
+def calcWithParen(expression):
+    i,ans,sign=0,0,1
+    stack = []
+    len_=len(expression)
+    while i < len_:
         if(expression[i].isdigit()):
-            num=expression[i]
-            while i + 1 < len(expression) and expression[i + 1].isdigit():
-                i += 1
-                num += expression[i]
-            ans += signed * int(num)#store the number to our answer and do the plus operation with whatever sign in front of a number
-        
-        ##determine the sign for the following number
+            num = expression[i]
+            while(i+1 < len_ and expression[i+1].isdigit() ):
+                num+=expression[i+1]
+                i+=1
+            ans+=int(num)*sign
         elif(expression[i]=="+"):
-            signed = 1
-        ##determine the sign for the following number
+            sign=1
         elif(expression[i]=="-"):
-            signed = -1
-        
-        ##push in the stack of whatever current result is, start new calculation after the "("
+            sign=-1
         elif(expression[i]=="("):
             stack.append(ans)
-            stack.append(signed)
-            ans, signed = 0,1
-        ##finish the calculation inside of the parethesis, then pop it out and do the calculatation with the previous result
+            stack.append(sign)
+            sign,ans=1,0
         elif(expression[i]==")"):
-            ans = ans * stack.pop() + stack.pop()
+            ans = stack.pop()*ans +stack.pop()
         i+=1
     return ans
 
-print(calculator2(expression_2))
+print(calcWithParen(expression_2))
--- Karat/Series-Treasure.py
+++ Karat/Series-Treasure.py
@@ -1,36 +1,44 @@
 ##########Test case
+from collections import defaultdict
+from tempfile import tempdir
+
+
 matrix=[
   ["1","1","1","1","0"],
   ["1","1","0","1","0"],
   ["1","1","0","1","0"],
   ["0","0","0","0","0"]
 ]
+board=[
+    [  1,  0,  0, 0, 0 ],
+    [  0, -1, -1, 0, 0 ],
+    [  0, -1,  0, 1, 0 ],
+    [ -1,  0,  0, 0, 0 ],
+    [  0,  1, -1, 0, 0 ],
+    [  0,  0,  0, 0, 0 ],    
+]
+
 ##########
 '''
 You are given a 2D array consisting of 1's and 0's and a tuple of starting coordinates in the matrix. 
 Return a list of the neighboring coordinates that are 0's NOT 1's.
 '''
 
-def main(matrix, start):
-    result=[]
-    x = start[0]
-    y = start[1]
-    findZeros(matrix, x, y, result)
-    for line in matrix:
-        print(line,'\n')
+def findZerosFourDirection(matrix, x, y):
+    result = []
+    def dfs(x,y):
+        if(x < 0 or y < 0 or x >= len(matrix) or y >= len(matrix[0]) or matrix[x][y]!= '0'):
+            return
+        matrix[x][y] = '1'
+        result.append([x,y])
+        dfs(x+1,y)
+        dfs(x-1,y)
+        dfs(x,y+1)
+        dfs(x,y-1)
+    dfs(x,y)
     return result
+#print(findZerosFourDirection(matrix,2,2))
 
-def findZeros(matrix, x, y, result):
-    if(x < 0 or y <0 or x>=len(matrix) or y>=len(matrix[0]) or matrix[x][y]!='0'):
-        return
-    result.append([x,y])
-    matrix[x][y]='#'
-    findZeros(matrix, x+1, y, result)
-    findZeros(matrix, x-1, y, result)
-    findZeros(matrix, x, y-1, result)
-    findZeros(matrix, x, y+1, result)
-    
-#print(main(matrix,[2,2]))
 
 '''
 You are given a 2D array consisting of 1's and 0's and a tuple of starting coordinates in the matrix. 
@@ -49,7 +57,6 @@ def main2(matrix, start):
     for line in matrix:
         print(line,'\n')
     return True
-
 def findZeros(matrix, x, y, result):
     if(x < 0 or y <0 or x>=len(matrix) or y>=len(matrix[0]) or matrix[x][y]!='0'):
         return
@@ -60,4 +67,43 @@ def findZeros(matrix, x, y, result):
     findZeros(matrix, x, y-1, result)
     findZeros(matrix, x, y+1, result)
     
-print(main2(matrix, [2,2]))
+#print(main2(matrix, [2,2]))
+
+def findShortestPath(start,end,matrix):
+    allPaths = defaultdict(list)
+    x,y = start[0], start[1]
+    xlen = len(matrix)
+    ylen = len(matrix[0])
+    remain = 0
+    path_ =[]
+    #count remains
+    for i in range(0, len(matrix)):
+        for j in range(0, len(matrix[0])):
+            if(matrix[i][j]==1):
+                remain+=1
+    
+    def dfs(x,y, path, remains):
+        if(x<0 or y<0 or x >= xlen or y >= ylen or matrix[x][y] == -1 or matrix[x][y] == 2):
+            return
+        path.append([x,y])
+        cur=matrix[x][y]
+        if(cur==1):
+            remains-=1
+        if(x==end[0] and y==end[1] and remains==0):
+            allPaths[len(path)].append(path.copy())
+            #backtrack
+            path.pop()
+            matrix[x][y]=cur
+            return
+        matrix[x][y]=2
+        dfs(x+1,y,path,remains)
+        dfs(x-1,y,path,remains)
+        dfs(x,y+1,path,remains)
+        dfs(x,y-1,path,remains)
+        #back track
+        path.pop()
+        matrix[x][y]=cur
+    dfs(start[0],start[1],path_,remain)
+    return(allPaths[min(allPaths)])
+print(findShortestPath([5,2],[2,0],board))
+    
