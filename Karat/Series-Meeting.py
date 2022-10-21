c33181e677a7db39cc9f9d27789b0d5ab03a0a11
szhan89 <zhangsiweiv@gmail.com>
2022-10-20 21:34:56 -0500

    fixed freeTime function

--- Karat/Series-Meeting.py
+++ Karat/Series-Meeting.py
@@ -5,13 +5,13 @@ meeting = [[1300, 1500], [930, 1200],[830, 845]]
 '''
 Given a list that contains start and end time for meetings, check if a meeting time can be inserted
 '''
-
-def freeTime(list_):
-    for start,end in list_:
-        if((start >= list[0] and start < list[1]) or ( end > list[0] and end <= list[1] ) or (start < list[0] and end > list[1]) ):
+def freeTime(list_,time):
+    for start, end in list_:
+        if((time[0]>end and time[0]<=start) or (time[1]>start and time[1]<=end) or (time[0]<=start and time[1]>=end)):
             return False
         return True
-    
+print(freeTime(meeting, [1450,1500]))
+
 '''
 Given a list that contains start and end time for meetings, return all its free space
 '''    
@@ -22,7 +22,7 @@ def merge(list_):
     for cur in list_:
         if(cur[0]<pre[1]):
             pre[0] = min(cur[0],pre[0])
-            pre[1] = min(cur[1],pre[1])
+            pre[1] = max(cur[1],pre[1])
         else:
             merged.append(pre)
             pre=cur
@@ -36,5 +36,4 @@ def merge(list_):
     if start < 2400:
         result.append([start, 2400])
     return result
-
 print(merge(meeting))
